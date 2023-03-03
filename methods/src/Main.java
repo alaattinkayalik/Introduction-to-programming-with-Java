7971105983405f989395a179f21e847d3eb0cb35
Alaattin <123412972+alaattinkayalik@users.noreply.github.com>
2023-03-04 01:51:40 +0300

    methods
    Why use methods? To reuse code: define the code once, and use it many times.

---
+++ methods/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Why use methods? To reuse code: define the code once, and use it many times.
+
+        myMethod();
+
+    }
+
+    static void myMethod(){
+        System.out.println("I Just got executed.");
+    }
+}
